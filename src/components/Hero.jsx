929d5416c469835ecc19fbe7a5166b5c112c391a
David Fernández <davidfernandezcomesana@gmail.com>
2023-03-04 16:48:05 +0100

    update responsive

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
     return (
         <section className="vg-page page-home" id="home">
             <div className="caption">
-                <div>
+                <div className='caption-text'>
                     <h3 className="text-[35px] sm:text-[20px] md:text-[35px] font-light">hola, soy</h3>
                     <h2 className="text-[60px] sm:text-[40px] md:text-[60px] font-bold"><span className='h1-cl'>D</span>avid</h2>
                     <h2 className="text-[60px] sm:text-[40px] md:text-[60px] font-bold"><span className='h1-cl'>F</span>ernández</h2>
